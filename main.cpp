847da8f58634ff86113572cf3e94f02c96725f8d
seoyeon544 <hongseoyeonee@gmail.com>
2025-10-28 20:20:48 +0900

    과제 제출

--- main.cpp
+++ main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
 #include "bird.h"
 
+class Duck : public Bird{
+    public:
+    Duck() : Bird("Duck"){
+        cry();
+    }
+
+    void cry() override{
+        std::cout<<"Quack!"<<std::endl;
+    }
+
+    void fly() override{
+        std::cout <<"This bird can fly." <<std::endl;
+    }
+
+    void swim() override{
+        std::cout <<"This bird can swim."<<std::endl;
+    }
+
+    ~Duck() override{
+        cry();
+    }
+};
+
+class Penguin : public Bird{
+    public:
+    Penguin() : Bird("Penguin") {
+        cry();
+    }
+
+    void cry() override {
+        std::cout << "Squawk!" << std::endl;
+    }
+
+    void fly() override {
+        std::cout << "This bird can't fly." << std::endl;
+    }
+
+    void swim() override{
+        std::cout<<"This bird can swim."<<std::endl;
+    }
+
+    ~Penguin() override {
+        cry();
+    }
+};
+
+class Eagle : public Bird{
+    public:
+    Eagle() : Bird("Eagle"){
+        cry();
+    }
+
+    void cry() override{
+        std::cout <<"Screech!"<<std::endl;
+    }
+
+    void fly() override{
+        std::cout<<"This bird can fly."<<std::endl;
+    }
+
+    void swim() override{
+        std::cout<<"This bird can't swim." <<std::endl;
+    }
+
+    ~Eagle() override{
+        cry();
+    }
+};
+
+
 int main() {
     Bird* birds[] = {
         new Duck(),
@@ -19,11 +89,9 @@ int main() {
         std::cout << "----------------------\n";
     }
 
-    // Clean up
     for (int i = 0; i < numBirds; ++i) {
         delete birds[i];
     }
 
     return 0;
-}
-
+};
